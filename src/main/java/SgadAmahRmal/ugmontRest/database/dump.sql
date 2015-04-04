e540864c92d19dab0db9d79c6755a8a63289c0ee
Romain <romain.malet@yahoo.fr>
2015-04-04 15:34:30 +0200

    Complete sql dump file

--- src/main/java/SgadAmahRmal/ugmontRest/database/dump.sql
+++ src/main/java/SgadAmahRmal/ugmontRest/database/dump.sql
@@ -1,43 +1,40 @@
 
 DROP TABLE if EXISTS film_salle;
-DROP TABLE if EXISTS film;
 DROP TABLE if EXISTS salle;
-
-CREATE TABLE film (
-	imdbID VARCHAR(255) PRIMARY KEY,
-	title VARCHAR(255),
-	year VARCHAR(255),
-	rated VARCHAR(255),
-	released VARCHAR(255),
-	runtime VARCHAR(255),
-	genre VARCHAR(255),
-	director VARCHAR(255),
-	writer VARCHAR(255),
-	actors VARCHAR(255),
-	plot CLOB,
-	language VARCHAR(255),
-	country VARCHAR(255),
-	awards VARCHAR(255),
-	poster VARCHAR(255),
-	metascore VARCHAR(255),
-	imdbRating VARCHAR(255),
-	imdbVotes VARCHAR(255),
-	ftype VARCHAR(255)
-);
 
 CREATE TABLE salle (
 	id BIGINT PRIMARY KEY,
 	name VARCHAR(255),
-	address VARCHAR(255),
 	city VARCHAR(255),
 	zipcode VARCHAR(255),
+	departement VARCHAR(255)
 );
 
 CREATE TABLE film_salle (
-	film_id VARCHAR(255) FOREIGN KEY (film_id) REFERENCES film(imdbID) ON UPDATE CASCADE ON DELETE CASCADE,
-	salle_id BIGINT FOREIGN KEY (salle_id) REFERENCES salle(id) ON UPDATE CASCADE ON DELETE CASCADE
+	salle_id BIGINT FOREIGN KEY (salle_id) REFERENCES salle(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	film_id VARCHAR(255)
 );
 
-/*
-INSERT INTO film VALUES
-*/
+ALTER TABLE film_salle ADD CONSTRAINT uk_film_salle UNIQUE(salle_id, film_id);
+
+INSERT INTO salle VALUES
+(1, "Utopia", "Toulouse", "31000", "31"),
+(2, "Moulin du Roc", "", "Niort", "79000", "79"),
+(3, "Pathé", "Prahecq", "79512", "79"),
+(4, "", "", "", ""),
+(5, "", "", "", ""),
+(6, "", "", "", ""),
+(7, "", "", "", ""),
+(8, "", "", "", ""),
+(9, "", "", "", ""),
+(10, "", "", "", ""),
+(11, "", "", "", ""),
+(12, "", "", "", ""),
+(13, "", "", "", ""),
+(14, "", "", "", ""),
+(15, "", "", "", ""),
+(16, "", "", "", ""),
+(17, "", "", "", ""),
+(18, "", "", "", ""),
+(19, "", "", "", ""),
+(20, "", "", "", "");
