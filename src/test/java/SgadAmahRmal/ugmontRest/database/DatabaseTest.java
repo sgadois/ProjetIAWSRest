ec06df4bf851a20be894d2701e05e73e17a8f901
Romain <romain.malet@yahoo.fr>
2015-04-05 11:17:16 +0200

    Patch sql errors in dump file Add a database test to check the dump is ok

---
+++ src/test/java/SgadAmahRmal/ugmontRest/database/DatabaseTest.java
@@ -0,0 +1,30 @@
+package SgadAmahRmal.ugmontRest.database;
+
+import static org.junit.Assert.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DatabaseTest {
+
+	private Database bdd;
+	
+	@Before
+	public void setUp() throws Exception {
+		bdd = Database.getInstance();
+	}
+
+	@Test
+	public void testStartDump() throws SQLException {
+		String utopia = null;
+		ResultSet res = bdd.getQuery("SELECT name FROM salle WHERE id=1");
+		if (res.next()) {
+			utopia = res.getString("name");
+		}
+		assertEquals("Utopia", utopia);
+	}
+
+}
