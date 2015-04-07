5089cd0dbf4f068e93bda95f59b536ba8b0233e7
abakarEgrey <egreabakar@yahoo.fr>
2015-04-07 02:17:52 +0200

    recuperation donnees

---
+++ src/main/java/SgadAmahRmal/ugmontRest/domain/Tuple.java
@@ -0,0 +1,18 @@
+package SgadAmahRmal.ugmontRest.domain;
+
+/**
+ * Created by mahamat on 07/04/15.
+ */
+public class Tuple<T, U>  {
+    T name;
+    U value;
+
+    public Tuple(T name, U value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public  T getName(){ return name;}
+    public U getValue(){ return value;}
+
+}
