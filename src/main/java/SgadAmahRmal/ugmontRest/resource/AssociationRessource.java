2887e63be17acf0b5324728c4c6085fd59704ba1
abakarEgrey <egreabakar@yahoo.fr>
2015-04-14 01:30:03 +0200

    correction de la methode isValidFilmId et reour d'une reponse adaptee

--- src/main/java/SgadAmahRmal/ugmontRest/resource/AssociationRessource.java
+++ src/main/java/SgadAmahRmal/ugmontRest/resource/AssociationRessource.java
@@ -1,11 +1,7 @@
 package SgadAmahRmal.ugmontRest.resource;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -14,6 +10,11 @@ import javax.ws.rs.core.Response;
 
 import SgadAmahRmal.ugmontRest.dao.ITheaterDao;
 import SgadAmahRmal.ugmontRest.domain.Theater;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * films resource
@@ -30,31 +31,52 @@ public class AssociationRessource {
      * @param theaterId theater id
      * @return a xml balise succes or fail
      */
-    @GET
+    @POST
     @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.APPLICATION_XML)
     @Path("{filmId}/{theaterId}")
     public Response storeFilmTheater(@PathParam("filmId") String filmId,
                                    @PathParam("theaterId") String theaterId) {
+        String output;
         if (!isValideFilmId(filmId) || !isValideSalleId(theaterId)) {
-            System.out.println(response.getStatus());
-            return response;
+            output = "<error> filmId ou theaterId invalid </error>";
+            return Response
+                    .ok(output, MediaType.APPLICATION_XML)
+                    .build();
         }
-        System.out.println(filmId + ", " + response.getStatus() + " , " + theaterId );
         dao.filmTheater(filmId, theaterId);
-
-        return response;
+        output = "<succes>The storage was successfully </succes>";
+        return Response
+                .ok(output, MediaType.APPLICATION_XML)
+                .build();
     }
 
     private boolean isValideFilmId(String filmId) {
+        boolean resultat = false;
+        Response response1 = null;
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://www.omdbapi.com/")
                 .queryParam("i", filmId)
                 .queryParam("r", "xml");
 
         response = target.request(MediaType.APPLICATION_XML).get();
+        response1 = target.request(MediaType.APPLICATION_XML).get();
+        BufferedReader br = new BufferedReader(new InputStreamReader((InputStream)response1.getEntity()));
+        String line = null;
+        try {
 
-        return (response.getStatus() == 200);
+            br.mark(response1.getLength() + 1);
+            while(((line = br.readLine()) != null) && !resultat) {
+                resultat = line.contains("response=\"False\"");
+            }
+            br.reset();
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return !resultat;
     }
 
     private boolean isValideSalleId(String theaterId) {
